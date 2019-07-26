7c342626f84526f5560ffcae5ed8692aa6767018
sunilkumarmaurya786693 <sunilkumarmaurya786693@gmail.com>
2019-07-26 20:13:45 +0530

    added router and controller

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require('express')
 const app=express();
 const port=8000
+
+app.use('/',require('./routes/urlindex'));
+
+
+
 app.listen(port,function(err){
     if(err)
     {console.log('err in running server',err);return;}
